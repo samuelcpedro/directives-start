1441958f1dd755a85649308f9fa1afa57b70f163
Samuel Pedro <samuelcpedro@gmail.com>
2018-12-12 16:33:09 -0100

    More about the Renderer
    More about the Renderer
    Section 7, Lecture 88

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -11,6 +11,14 @@ export class BetterHighlightDirective implements OnInit {
     // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     // Add 'implements OnInit' to the class.
     this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
+    /**
+     *
+     * In the last lecture, we used the Angular Renderer class to change the style of a HTML element.
+     * As explained in that lecture, you should use the Renderer for any DOM manipulations.
+     * Of course, you can do more than simply change the styling of an element via setStyle().
+     * Learn more about the available Renderer methods here. https://angular.io/api/core/Renderer2
+     *
+     */
   }
 
 }
